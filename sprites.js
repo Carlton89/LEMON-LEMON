4b6d42049b6d95ecd44588cb9a6f8844ff5c11f1
Carlton89 <m.licordari@gmail.com>
2025-04-18 18:07:18 +0200

    Add files via upload

---
+++ sprites.js
@@ -0,0 +1,15 @@
+
+const SPRITES = {
+  redCar:     { x: 0,   y: 90,  w: 64, h: 64 },
+  yellowCar:  { x: 192, y: 90,  w: 64, h: 64 },
+  palmLeft:   { x: 0,   y: 0,   w: 64, h: 90 },
+  palmRight:  { x: 192, y: 0,   w: 64, h: 90 },
+  banner:     { x: 64,  y: 0,   w: 128, h: 64 },
+  roadBlock:  { x: 0,   y: 154, w: 64, h: 64 },
+  bg1:        { x: 0,   y: 218, w: 256, h: 64 },
+  bg2:        { x: 0,   y: 282, w: 256, h: 64 },
+  bg3:        { x: 0,   y: 346, w: 256, h: 64 },
+  lemon:      { x: 0,   y: 410, w: 32,  h: 32 },
+  btnLeft:    { x: 64,  y: 410, w: 48,  h: 48 },
+  btnRight:   { x: 112, y: 410, w: 48,  h: 48 }
+};
