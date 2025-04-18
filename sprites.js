5c091e17e1c455c3fe73dbcce8b9e841b33261bb
Carlton89 <m.licordari@gmail.com>
2025-04-18 18:50:00 +0200

    Delete sprites.js

--- sprites.js
+++
@@ -1,7 +0,0 @@
-
-const SPRITES = {
-  redCar:   { x: 0,   y: 0,   w: 64, h: 64 },
-  palm:     { x: 64,  y: 0,   w: 64, h: 64 },
-  road:     { x: 128, y: 0,   w: 64, h: 64 },
-  lemon:    { x: 192, y: 0,   w: 32, h: 32 }
-};
