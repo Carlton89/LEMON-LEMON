ad1f53015df103ff2740c1c9d3c52e47366421b9
Carlton89 <m.licordari@gmail.com>
2025-04-18 18:22:52 +0200

    Update sprites.js

--- sprites.js
+++ sprites.js
@@ -1,11 +1,9 @@
 
 const SPRITES = {
   redCar:     { x: 0,   y: 90,  w: 64, h: 64 },
-  yellowCar:  { x: 192, y: 90,  w: 64, h: 64 },
   palmLeft:   { x: 0,   y: 0,   w: 64, h: 90 },
   palmRight:  { x: 192, y: 0,   w: 64, h: 90 },
   banner:     { x: 64,  y: 0,   w: 128, h: 64 },
-  roadBlock:  { x: 0,   y: 154, w: 64, h: 64 },
   bg1:        { x: 0,   y: 218, w: 256, h: 64 },
   bg2:        { x: 0,   y: 282, w: 256, h: 64 },
   bg3:        { x: 0,   y: 346, w: 256, h: 64 },
